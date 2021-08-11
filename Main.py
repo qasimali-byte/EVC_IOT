66a4fbb42c3a8eda86402a859277ef3b633c8bdd
bili <qasimwarraich968@gmail.com>
2021-08-11 11:28:18 +0500

    Initial Commit

--- Main.py
+++ Main.py
@@ -1,5 +1,3 @@
-import time
-#Setup a loop to send Temperature values at fixed intervals in seconds
 import pyrebase
 Config = {
     'apiKey': "AIzaSyCdL3NSUDBQmwJUeKg3JLmrdqJtpYb01BY",
@@ -13,12 +11,9 @@ Config = {
 }
 firebase=pyrebase.initialize_app(Config)
 db=firebase.database()
-
-
 def stream_handler(message):
     # print(message["event"]) # put
     # print(message["path"]) # /-K7yGTTEp7O549EzTYtI
     print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
-
 
 my_stream = db.child("Temperatures").stream(stream_handler)
